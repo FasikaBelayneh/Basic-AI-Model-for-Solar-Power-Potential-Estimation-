489a044cb144bee4262823f38345507e2533bad2
Fasika Belayneh <fafibelayneh@gmail.com>
2025-04-25 01:09:05 +0300

    updating views.py for making prediction using the features and loading the trained model

--- solar_estimation_project/estimation_app/views.py
+++ solar_estimation_project/estimation_app/views.py
@@ -1,9 +1,48 @@
+import joblib
+import numpy as np
 from django.shortcuts import render
+from .forms import SolarInputForm
 
-# estimation_app/views.py
-from django.shortcuts import render
-
-def estimate_solar_potential(request):
-    # This view will handle both displaying the form (GET)
-    # and processing the form submission (POST) later
-    return render(request, 'estimation_app/estimation_form.html', {})
+def predict_solar(request):
+    if request.method == 'POST':
+        form = SolarInputForm(request.POST)
+        if form.is_valid():
+            # Load trained model
+            model = joblib.load('solar_app/ml_model/solar_potential_model.pkl')
+            
+            # Create feature array
+            features = np.array([
+                [
+                    2023,  # Year
+                    form.cleaned_data['month'],
+                    form.cleaned_data['avg_temp'],
+                    form.cleaned_data['avg_temp'] + 5,  # Max temp (example)
+                    form.cleaned_data['avg_temp'] - 5,  # Min temp (example)
+                    form.cleaned_data['precipitation'],
+                    form.cleaned_data['humidity'],
+                    4.5,  # Wind speed (default)
+                    form.cleaned_data['cloud_cover'],
+                    415,  # CO2 concentration (typical value)
+                    form.cleaned_data['latitude'],
+                    form.cleaned_data['longitude'],
+                    150,  # Altitude (default)
+                    10,   # Proximity to water
+                    0.5,  # Urbanization index
+                    0.7,  # Vegetation index
+                    0.2,  # ENSO index
+                    25,   # Particulate matter
+                    28    # Sea surface temp
+                ]
+            ])
+            
+            # Make prediction
+            prediction = model.predict(features)
+            
+            return render(request, 'solar_app/result.html', {
+                'prediction': round(prediction[0], 2)
+            })
+    
+    else:
+        form = SolarInputForm()
+    
+    return render(request, 'solar_app/input_form.html', {'form': form})
