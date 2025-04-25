e47854fae753d76d5f64f0780264c0eeab3244f2
Fasika Belayneh <fafibelayneh@gmail.com>
2025-04-25 03:44:44 +0300

    setting up the baseline model

--- solar_estimation_project/estimation_app/models.py
+++ solar_estimation_project/estimation_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
+from django.contrib.gis.db import models as gis_models
 
-# Create your models here.
+class RegionalAverage(models.Model):
+    location = gis_models.PointField()
+    average_irradiance = models.FloatField()
+    month = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class SolarIncentive(models.Model):
+    country = models.CharField(max_length=2)
+    region = models.CharField(max_length=100)
+    incentives = models.JSONField()
+    effective_from = models.DateField()
+    effective_to = models.DateField()
+
+class SolarReport(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    report_data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = gis_models.PointField()
+    month = models.IntegerField()
